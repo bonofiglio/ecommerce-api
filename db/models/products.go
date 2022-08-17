3ab15d49434303d1c4c836cb67c7bccefdfcf976
Daniel Bonofiglio <dev@dan.uy>
2022-08-17 04:24:38 -0300

    fix(products): fix bun binding for description field

--- db/models/products.go
+++ db/models/products.go
@@ -9,7 +9,7 @@ type Product struct {
 	Slug        string  `bun:"slug,unique,notnull"`
 	Sku         string  `bun:"sku,unique,notnull"`
 	Name        string  `bun:"name,notnull"`
-	Description string  `bun:"name"`
+	Description string  `bun:"description"`
 	Price       float64 `bun:"price,notnull"`
 	Active      bool    `bun:"active,notnull"`
 }
