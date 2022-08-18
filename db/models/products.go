f4cc6d48960edc3d599c0a49d5448de74ba2a641
Daniel Bonofiglio <dev@dan.uy>
2022-08-18 02:13:50 -0300

    feat: add json field names to product model

--- db/models/products.go
+++ db/models/products.go
@@ -5,11 +5,11 @@ import "github.com/uptrace/bun"
 type Product struct {
 	bun.BaseModel `bun:"table:products"`
 
-	ID          int64   `bun:"id,pk,autoincrement"`
-	Slug        string  `bun:"slug,unique,notnull"`
-	Sku         string  `bun:"sku,unique,notnull"`
-	Name        string  `bun:"name,notnull"`
-	Description string  `bun:"description"`
-	Price       float64 `bun:"price,notnull"`
-	Active      bool    `bun:"active,notnull"`
+	ID          int64   `bun:"id,pk,autoincrement" json:"id"`
+	Slug        string  `bun:"slug,unique,notnull" json:"slug"`
+	Sku         string  `bun:"sku,unique,notnull" json:"sku"`
+	Name        string  `bun:"name,notnull" json:"name"`
+	Description string  `bun:"description" json:"description"`
+	Price       float64 `bun:"price,notnull" json:"price"`
+	Active      bool    `bun:"active,notnull" json:"active"`
 }
