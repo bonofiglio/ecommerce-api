6836c6a2f3003ab8abf7d113c6ce4cda41a33529
Daniel Bonofiglio <dev@dan.uy>
2022-08-24 02:19:35 -0300

    refactor(server): move POST /users to POST /sign-up

---
+++ server/routes/sign-up/index.go
@@ -0,0 +1,54 @@
+package signup
+
+import (
+	"ecommerceapi/db/models"
+	"ecommerceapi/lib"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/uptrace/bun"
+)
+
+func post(c *echo.Context, db *bun.DB) error {
+	ctx := (*c).Request().Context()
+
+	user := models.User{}
+
+	if err := (*c).Bind(&user); err != nil {
+		return lib.CreateNewResponseError(400, err.Error())
+	}
+
+	// Unset forbidden fields from the request body in case they were sent in the request
+	user.ID = 0
+	user.CreatedAt = time.Time{}
+	user.UpdatedAt = time.Time{}
+	user.Orders = []*models.Order{}
+	user.Active = true
+
+	// Validate the request
+	if err := (*c).Validate(user); err != nil {
+		return err
+	}
+
+	// Generate a random salt to hash the password with
+	user.Salt = lib.GenerateRandomString(64)
+
+	// Hash the password with the salt
+	user.Password = lib.HashPassword(user.Password, user.Salt)
+
+	// Insert the user into the database
+	if _, err := db.NewInsert().Model(&user).Exec(ctx); err != nil {
+		return lib.CreateNewResponseError(500, err.Error())
+	}
+
+	// Extract the fields of the safe user from the user struct before returning it
+	safeUser := user.SafeUser
+
+	return (*c).JSON(200, safeUser)
+}
+
+func InitRoutes(app *echo.Echo, db *bun.DB) {
+	(*app).POST("/sign-up", func(c echo.Context) error {
+		return post(&c, db)
+	})
+}
